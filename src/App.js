39ce575fa69e54448d1566e7060c3c74ed9f9a55
Simon Muia <simonmuia38@gmail.com>
2023-02-01 14:12:51 +0300

    defined skelleton for the app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 
 import './App.css';
+import React from "react";
+import Home from './pages/Home'
+import List from './list/List'
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Routes,
+  Route,
+  Link,
+  BrowserRouter,
+} from "react-router-dom";
+import Hotel from './hotel/Hotel';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <h1 className='text-red-500'>Hello from react</h1>
+    <BrowserRouter>
+    <div>
+     
+      <Routes>
+        <Route path='' element={<Home/>} />
+        <Route path='/hotels' element={<List/>} />
+        <Route path='/hotels/:id' element={<Hotel/>} />
+      </Routes>
     </div>
+    
+    </BrowserRouter>
   );
 }
 
