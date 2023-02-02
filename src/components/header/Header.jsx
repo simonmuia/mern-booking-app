456395bfe309e9fa55539c4c84c84c3fb81efda5
Simon Muia <simonmuia38@gmail.com>
2023-02-02 10:46:58 +0300

    Added usestate on header list for hover effect

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,28 +1,39 @@
-import React from "react";
-import Categories from "./HeaderItem";
+import React, { useState } from "react";
+import Categories from "../../utils/HeaderItem";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-  
-const Header = () => {
-  return (
-    // Define  header Wrapper
-    <div className="flex justify-center pb-10 px-10 pt-10 bg-[#003580]" >
-      {/* Define Header Container */}
-      <div className="text-white w-max max-w-[1024px] mt-[20px] mb-[100px]">
+import './header.css'
 
+const Header = () => {
+  //add useState hook to label selected link
+  const [selectedHeaderItem, setselectedHeaderItem] = useState("");
+   return (
+    // Define  header Wrapper
+    <div className="flex justify-center pb-10 px-10 pt-10 bg-[#003580]">
+      {/* Define Header Container */}
+      <div className=" w-max max-w-[1024px] mt-[20px] mb-[100px]">
         {/* Header List */}
         <div className="flex gap-10">
-             {/*Header list item  */}
+          {/*Header list item  */}
 
-      {Object.entries(Categories).map(([key, { icon, title }]) => (
-        <div  className="flex p-[10px] items-center gap-[10px] cursor-pointer hover:border-white" key={key}>
-          <FontAwesomeIcon   icon={icon} />
-          <span>{title}</span>
+          {Object.entries(Categories).map(([key, { icon, title }]) => (
+            <div
+              onClick={() => {
+                setselectedHeaderItem(title);
+              }}
+             
+              className={
+                title === selectedHeaderItem
+                  ? "headerlist-item rounded-[20px] text-white flex p-[10px] items-center gap-[10px] cursor-pointer"
+                  : `p-[11px] rounded-[20px] text-white flex items-center gap-[10px] cursor-pointer hover:p-[10px] hover:border-white hover:border-[1px]`
+              }
+              key={key}
+            >
+              <FontAwesomeIcon icon={icon} />
+              <span>{title}</span>
+            </div>
+          ))}
         </div>
-      ))}
-        </div>
-
       </div>
-     
     </div>
   );
 };
